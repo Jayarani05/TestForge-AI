56ad096f6ce0dc934cde44da18c79d089d87f13d
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-06 02:14:21 +0530

    Integrate DeepSeek model using OpenRouter API

--- Backend/App/llm_services/deepseek_service.py
+++ Backend/App/llm_services/deepseek_service.py
@@ -1,24 +1,112 @@
-from app.llm_services.base_llm import BaseLLM
+from openai import OpenAI
+
+from app.config import DEEPSEEK_API_KEY
 
 
-class DeepSeekService(BaseLLM):
+class DeepSeekService:
+
+
+    def __init__(self):
+
+        self.client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=DEEPSEEK_API_KEY
+        )
 
 
     def generate_tests(
         self,
-        requirement: str
+        requirement
     ):
 
-        return {
 
-            "model": "DeepSeek",
+        try:
 
-            "tests": [
+            response = (
+                self.client
+                .chat
+                .completions
+                .create(
 
-                "Security validation",
+                    model=
+                    "deepseek/deepseek-chat",
 
-                "Unauthorized access testing",
+                    messages=[
+                        {
+                            "role":
+                            "system",
 
-                "Failure scenario analysis"
+                            "content":
+                            "You are a senior QA security engineer"
+                        },
+
+
+                        {
+                            "role":
+                            "user",
+
+                            "content":
+                            f"""
+                            Generate QA test cases:
+
+                            {requirement}
+
+                            Focus on:
+                            security testing,
+                            edge cases,
+                            failures
+                            """
+                        }
+                    ]
+                )
+            )
+
+
+            result = (
+                response
+                .choices[0]
+                .message
+                .content
+            )
+
+
+            tests = [
+
+                line.strip()
+
+                for line in result.split("\n")
+
+                if line.strip()
             ]
-        }
+
+
+            return {
+
+                "model":
+                "DeepSeek",
+
+                "status":
+                "success",
+
+                "tests":
+                tests
+            }
+
+
+        except Exception as error:
+
+            return {
+
+                "model":
+                "DeepSeek",
+
+                "status":
+                "failed",
+
+                "error":
+                repr(error),
+
+                "tests":[
+                    "DeepSeek fallback activated"
+                ]
+            }
