79f1e5ada9240dbea58a67d5e06a1bd6c74111c9
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-06 11:29:58 +0530

    Upgrade automation generator with AI code creation

--- Backend/App/code_generators/automation_generator.py
+++ Backend/App/code_generators/automation_generator.py
@@ -1,4 +1,16 @@
+from app.llm_services.code_generation_service import (
+    CodeGenerationService
+)
+
+
 class AutomationGenerator:
+
+
+    def __init__(self):
+
+        self.code_service = (
+            CodeGenerationService()
+        )
 
 
 
@@ -9,166 +21,18 @@ class AutomationGenerator:
     ):
 
 
-        if language.lower() == "python":
+        generated_code = (
 
-            return self.generate_python(
-                test_cases
+            self.code_service
+            .generate_code(
+
+                test_cases,
+
+                language
+
             )
 
-
-        if language.lower() == "javascript":
-
-            return self.generate_javascript(
-                test_cases
-            )
-
-
-        if language.lower() == "java":
-
-            return self.generate_java(
-                test_cases
-            )
-
-
-
-        return (
-            "Unsupported language"
         )
 
 
-
-
-    def generate_python(
-        self,
-        test_cases
-    ):
-
-
-        code = """
-
-from selenium import webdriver
-
-
-def test_generated_case():
-
-
-    driver = webdriver.Chrome()
-
-
-"""
-
-
-        for category, tests in test_cases.items():
-
-
-            for test in tests:
-
-
-                code += f'''
-
-    # {test["title"]}
-
-    assert True
-
-'''
-
-
-        code += """
-
-    driver.quit()
-
-"""
-
-
-        return code
-
-
-
-
-
-    def generate_javascript(
-        self,
-        test_cases
-    ):
-
-
-        code = """
-
-import { test, expect } from '@playwright/test';
-
-
-test('AI generated test', async ({ page }) => {
-
-
-"""
-
-
-        for category, tests in test_cases.items():
-
-
-            for test in tests:
-
-
-                code += f'''
-
-    // {test["title"]}
-
-    expect(true).toBeTruthy();
-
-'''
-
-
-        code += """
-
-});
-
-"""
-
-
-        return code
-
-
-
-
-
-    def generate_java(
-        self,
-        test_cases
-    ):
-
-
-        code = """
-
-public class GeneratedTest {
-
-
-    public void testCase(){
-
-
-"""
-
-
-        for category, tests in test_cases.items():
-
-
-            for test in tests:
-
-
-                code += f'''
-
-        // {test["title"]}
-
-
-'''
-
-
-        code += """
-
-    }
-
-}
-
-"""
-
-
-        return code
+        return generated_code
