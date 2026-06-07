f911928088ee5eb20843a19bd4c9607b9021be1c
Harshini T <harshini2709005@gmail.com>
2026-06-07 12:29:53 +0530

    Create CICD t interfaces

---
+++ Frontend/src/pages/CICD.jsx
@@ -0,0 +1,37 @@
+function CICD(){
+
+
+return (
+
+<div className="bg-black min-h-screen p-10">
+
+
+<h1 className="text-white text-3xl">
+
+CI/CD Generator
+
+</h1>
+
+
+<div className="
+bg-gray-900
+text-white
+p-6
+rounded-xl
+mt-8
+">
+
+.github/workflows/python-app.yml
+
+
+</div>
+
+
+</div>
+
+);
+
+}
+
+
+export default CICD;
