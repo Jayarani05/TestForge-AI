3e1b5551a002f21918afc37f7d9a6279c9e33343
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-07 15:23:03 +0530

    Connect project workspace with backend API

--- Frontend/src/pages/Projects.jsx
+++ Frontend/src/pages/Projects.jsx
@@ -1,58 +1,192 @@
+import { useEffect, useState } from "react";
+
+import api from "../api/axios";
+
 import ProjectCard from "../components/ProjectCard";
 
 
 function Projects(){
 
 
-const projects=[
+const [projects,setProjects] = useState([]);
 
-{
-name:"Food Delivery Testing",
-description:"Spring Boot application testing",
-technology:"Spring Boot + React"
-},
 
-{
-name:"Ecommerce QA",
-description:"Automation test platform",
-technology:"MERN"
-}
+const [form,setForm] = useState({
 
-];
+name:"",
+
+description:"",
+
+technology:""
+
+});
+
+
+
+
+useEffect(()=>{
+
+loadProjects();
+
+},[]);
+
+
+
+
+const loadProjects = async()=>{
+
+
+const response = await api.get(
+
+"/projects"
+
+);
+
+
+setProjects(response.data);
+
+
+};
+
+
+
+
+
+const createProject = async()=>{
+
+
+await api.post(
+
+"/projects",
+
+form
+
+);
+
+
+setForm({
+
+name:"",
+description:"",
+technology:""
+
+});
+
+
+loadProjects();
+
+
+};
+
+
+
 
 
 return (
 
-<div className="
-min-h-screen
-bg-black
-p-10
-">
+<div className="min-h-screen bg-black p-10">
 
 
-<h1 className="
-text-white
-text-3xl
-font-bold
-">
+<h1 className="text-white text-3xl font-bold">
 
 Projects
 
 </h1>
 
 
-<button className="
-bg-blue-600
-text-white
-px-5
-py-3
-rounded-lg
-mt-6
-">
 
-+ Create Project
+<div className="mt-8 space-y-4">
+
+
+<input
+
+placeholder="Project Name"
+
+className="bg-gray-900 text-white p-3 rounded w-full"
+
+value={form.name}
+
+onChange={(e)=>
+
+setForm({
+
+...form,
+
+name:e.target.value
+
+})
+
+}
+
+/>
+
+
+
+<input
+
+placeholder="Description"
+
+className="bg-gray-900 text-white p-3 rounded w-full"
+
+value={form.description}
+
+onChange={(e)=>
+
+setForm({
+
+...form,
+
+description:e.target.value
+
+})
+
+}
+
+/>
+
+
+
+<input
+
+placeholder="Technology"
+
+className="bg-gray-900 text-white p-3 rounded w-full"
+
+value={form.technology}
+
+onChange={(e)=>
+
+setForm({
+
+...form,
+
+technology:e.target.value
+
+})
+
+}
+
+/>
+
+
+
+
+<button
+
+onClick={createProject}
+
+className="bg-blue-600 text-white px-6 py-3 rounded"
+
+>
+
+Create Project
 
 </button>
+
+
+</div>
+
+
 
 
 
@@ -66,16 +200,21 @@ mt-10
 
 {
 
+
 projects.map(
 
-(project,index)=>(
+(project)=>(
 
 
 <ProjectCard
 
-key={index}
+key={project.id}
 
-{...project}
+name={project.name}
+
+description={project.description}
+
+technology={project.technology}
 
 />
 
@@ -92,6 +231,7 @@ key={index}
 </div>
 
 );
+
 
 }
 
