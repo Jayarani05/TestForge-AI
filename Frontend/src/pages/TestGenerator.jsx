b94f632265a6c74d2ace8b1806a1817544d0336c
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-07 15:26:15 +0530

    Connect AI test generation API

--- Frontend/src/pages/TestGenerator.jsx
+++ Frontend/src/pages/TestGenerator.jsx
@@ -1,16 +1,108 @@
+import { useState } from "react";
+
+import api from "../api/axios";
+
 import ResultViewer from "../components/ResultViewer";
+
 
 
 function TestGenerator(){
 
 
+const [story,setStory] = useState("");
+
+
+const [language,setLanguage] = useState(
+    "Python"
+);
+
+
+const [framework,setFramework] = useState(
+    "Pytest"
+);
+
+
+const [loading,setLoading] =
+useState(false);
+
+
+const [result,setResult] =
+useState(null);
+
+
+
+
+
+const generateTests = async()=>{
+
+
+try{
+
+
+setLoading(true);
+
+
+
+const response = await api.post(
+
+"/tests/generate",
+
+{
+
+user_story:story,
+
+output_type:"test_cases",
+
+language,
+
+framework,
+
+project_context:""
+
+}
+
+);
+
+
+
+setResult(
+
+response.data.agent_result
+
+);
+
+
+
+}
+
+catch(error){
+
+
+console.log(error);
+
+
+alert(
+"Generation failed"
+);
+
+
+}
+
+
+setLoading(false);
+
+
+};
+
+
+
+
+
+
 return (
 
-<div className="
-min-h-screen
-bg-black
-p-10
-">
+
+<div className="min-h-screen bg-black p-10">
 
 
 <h1 className="
@@ -25,7 +117,12 @@ AI Test Generator
 
 
 
+
 <textarea
+
+value={story}
+
+onChange={(e)=>setStory(e.target.value)}
 
 placeholder="Enter user story..."
 
@@ -42,26 +139,39 @@ rounded-xl
 />
 
 
-<div className="
-flex
-gap-5
-mt-5
-">
 
 
-<select className="
+
+<div className="flex gap-5 mt-5">
+
+
+<select
+
+value={language}
+
+onChange={(e)=>setLanguage(e.target.value)}
+
+className="
 bg-gray-900
 text-white
 p-3
 rounded
-">
+"
+
+>
+
 
 <option>
+
 Python
+
 </option>
 
+
 <option>
+
 Java
+
 </option>
 
 
@@ -69,20 +179,37 @@ Java
 
 
 
-<select className="
+
+
+<select
+
+value={framework}
+
+onChange={(e)=>setFramework(e.target.value)}
+
+className="
 bg-gray-900
 text-white
 p-3
 rounded
-">
+"
+
+>
+
 
 <option>
+
 Pytest
+
 </option>
 
+
 <option>
+
 Selenium
+
 </option>
+
 
 </select>
 
@@ -91,35 +218,73 @@ Selenium
 
 
 
-<button className="
+
+
+<button
+
+onClick={generateTests}
+
+className="
 bg-blue-600
 text-white
 px-6
 py-3
 rounded-lg
 mt-6
-">
+"
 
-Generate Tests
+>
+
+
+{
+
+loading
+?
+"Generating..."
+:
+"Generate Tests"
+
+
+}
+
 
 </button>
 
 
+
+
+
+{
+
+result &&
+
 <ResultViewer
 
-title="Generated Tests"
+title="Generated Test Cases"
 
-content="AI output appears here"
+content={
+
+JSON.stringify(
+result,
+null,
+2
+)
+
+}
 
 />
 
+}
+
 
 </div>
+
 
 );
 
 
 }
+
 
 
 export default TestGenerator;
