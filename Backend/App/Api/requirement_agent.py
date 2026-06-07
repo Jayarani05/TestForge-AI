96462359bcfb85070acdc04192c412ab56b83b93
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-06-07 22:33:09 +0530

    Add requirement intelligence agent

---
+++ Backend/App/Api/requirement_agent.py
@@ -0,0 +1,76 @@
+import json
+import google.generativeai as genai
+
+from app.core.config import settings
+
+
+genai.configure(
+    api_key=settings.GEMINI_API_KEY
+)
+
+
+
+def analyze_requirement(user_story:str):
+
+
+    model = genai.GenerativeModel(
+        "gemini-1.5-flash"
+    )
+
+
+    prompt=f"""
+
+
+You are a senior software requirement analyst.
+
+Analyze this user story:
+
+{user_story}
+
+
+Return ONLY JSON:
+
+{{
+ "functional_requirements":[
+
+ ],
+
+ "non_functional_requirements":[
+
+ ],
+
+ "sentiment":{{
+    "type":"",
+    "confidence":0,
+    "reason":""
+ }},
+
+ "risk_analysis":{{
+    "risk_level":"",
+    "risks":[]
+ }}
+}}
+
+
+"""
+
+
+    response=model.generate_content(
+        prompt
+    )
+
+
+    text=response.text
+
+
+    text=text.replace(
+        "```json",
+        ""
+    ).replace(
+        "```",
+        ""
+    ).strip()
+
+
+
+    return json.loads(text)
