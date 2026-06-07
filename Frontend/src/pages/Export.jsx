18ae32eb609e361f952c9752a503bfd408a2b1af
Harshini T <harshini2709005@gmail.com>
2026-06-07 12:31:04 +0530

    Create  export interfaces

---
+++ Frontend/src/pages/Export.jsx
@@ -0,0 +1,43 @@
+function Export(){
+
+
+return (
+
+<div className="bg-black min-h-screen p-10">
+
+
+<h1 className="text-white text-3xl">
+
+Export Center
+
+</h1>
+
+
+<div className="flex gap-5 mt-8">
+
+
+<button className="bg-blue-600 text-white p-4 rounded">
+
+Download PDF
+
+</button>
+
+
+<button className="bg-green-600 text-white p-4 rounded">
+
+Download Excel
+
+</button>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
+
+
+export default Export;
